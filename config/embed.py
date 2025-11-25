0d735fd50eef028a2f41fe6107617f44fe954c0d
Kysnez4 <kysnez4@gmail.com>
2025-11-25 13:06:12 +0300

    Уменьшение нагрузки на CPU, за счет округления очень маленьких чисел до 0. Явно указал модели использовать 32 битные числа, для уменьшения потребления памяти. Это все работает только на большом количестве текста.
    было 24 сек, а стало 20 сек или 19 сек

--- config/embed.py
+++ config/embed.py
@@ -5,12 +5,18 @@ from transformers import AutoModel
 
 class EmbedConfig:
     torch.set_num_threads(4)
+    torch.set_flush_denormal(True)
 
     MODEL_ID = "jinaai/jina-embeddings-v3"
+    MODEL_ID = "./jina-embeddings-v3"
     DEFAULT_DIMS = 512
     BATCH_SIZE = 8
     ROUND_DECIMALS = 6
     MAX_INPUT_CHARS = 32_000
 
     device = torch.device("cpu")
-    model = AutoModel.from_pretrained(MODEL_ID, revision="f1944de", trust_remote_code=True).eval().to(device)
+    model = AutoModel.from_pretrained(MODEL_ID,
+                                      revision="f1944de",
+                                      trust_remote_code=True,
+                                      torch_dtype=torch.float32,
+                                      ).eval().to(device)
